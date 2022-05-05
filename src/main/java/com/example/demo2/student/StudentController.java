a1665eaa9d4454b108878cd34d6e8b7ff1e60ee6
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2022-05-05 20:54:24 +0800

    Update StudentController.java

--- src/main/java/com/example/demo2/student/StudentController.java
+++ src/main/java/com/example/demo2/student/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
+@CrossOrigin("http://localhost:3000/")
 public class StudentController {
   private final StudentService studentService;
 
